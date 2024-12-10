79214a6fff798a87c18a3029911115a9cdeed1e6
literat <tomas@litera.me>
2024-12-10 13:53:55 +0100

    Add support for excluding words from capitalization
    refs #13

--- index.js
+++ index.js
@@ -2,36 +2,57 @@ import { lintRule } from 'unified-lint-rule'
 import { visit } from 'unist-util-visit'
 
 import lowerCaseWords from './lib/lowerCaseWords.js'
-import { capitalizeWord, isUpperCase } from './lib/utils.js'
+import { capitalizeWord, isUpperCase, isWordExcluded } from './lib/utils.js'
 
 const cache = {}
 
 export function fixTitle(title, options) {
-  const correctTitle = title.replace(/[^\s-]+/g, (word, index) => {
-
-    // If the word is already in uppercase, return it as is.
-    if (isUpperCase(word)) {
+  // Split the title into words by spaces
+  const correctTitle = title.replace(/[^\s]+/g, (word, wordPositionInTitle) => {
+    if (isWordExcluded(word, options)) {
       return word
     }
 
-    // If the word is not the first word in the title and should be lowercase, return it in lowercase.
-    const lowerCaseWord = word.toLowerCase()
-    if (index !== 0 && [...lowerCaseWords, ...(options.lowerCaseWords ?? [])].includes(lowerCaseWord)) {
-      return lowerCaseWord
-    }
-
-    // Checking the first letter of a word is not capitalized.
-    if (!isUpperCase(word.charAt(0))) {
-      return capitalizeWord(word)
-    }
-
-    return word
+    // Split hyphenated word to words by hyphens
+    // Applies word correction base on position in the title and position in the hyphenated word
+    return word.replace(/[^-]+/g, (hyphenatedWord, hyphenatedWordPosition) =>
+      correctWord(
+        hyphenatedWord,
+        hyphenatedWordPosition + wordPositionInTitle,
+        options
+      )
+    )
   })
 
   // Putting correct title in the cache for prevent handling the same titles in other docs.
   cache[correctTitle] = correctTitle
 
   return correctTitle
+}
+
+function correctWord(word, wordPosition, options = {}) {
+  // If the word is already in uppercase, return it as is.
+  if (isUpperCase(word)) {
+    return word
+  }
+
+  // If the word is not the first word in the title and should be lowercase, return it in lowercase.
+  const lowerCaseWord = word.toLowerCase()
+  if (
+    wordPosition !== 0 &&
+    [...lowerCaseWords, ...(options.lowerCaseWords ?? [])].includes(
+      lowerCaseWord
+    )
+  ) {
+    return lowerCaseWord
+  }
+
+  // Checking the first letter of a word is not capitalized.
+  if (!isUpperCase(word.charAt(0))) {
+    return capitalizeWord(word)
+  }
+
+  return word
 }
 
 function headingCapitalization(tree, file, options = {}) {
